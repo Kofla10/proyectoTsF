275b39185cc3aca78f2742737c3f78b2a8f23e2f
nkescobar <juank_milo94@hotmail.com>
2019-10-04 18:00:59 -0500

    view

--- src/app/pages/categoria/categoria.page.ts
+++ src/app/pages/categoria/categoria.page.ts
@@ -6,10 +6,10 @@ import {
   Validators
 } from '@angular/forms';
 // para obtener los campos del formulario
-import { Categoria } from '../../interfaces/categoria';
+import { Categoria } from '../../model/categoria';
 
 // importamos el servicio
-import {ConexionService} from '../../services/conexion.service';
+import { ConexionService } from '../../services/conexion.service';
 
 @Component({
   selector: 'app-categoria',
@@ -21,9 +21,12 @@ export class CategoriaPage implements OnInit {
   form: FormGroup;
   categoria = new Categoria();
 
-  constructor(private formbuilder: FormBuilder, private ser: ConexionService) {
-    this.ser.getMarketing();
+  constructor(
+    private formbuilder: FormBuilder,
+    private ser: ConexionService
+  ) {
 
+    this.ser.listaCategoria();
   }
 
   ngOnInit() {
@@ -32,10 +35,27 @@ export class CategoriaPage implements OnInit {
 
   initCategoria() {
     this.form = this.formbuilder.group({
-      'idCategoria': [this.categoria.idCategoria, [Validators.required]],
+      // 'idCategoria': [this.categoria.idCategoria, [Validators.required]],
       'descripcion': [this.categoria.descripcion, [Validators.required]],
       'nombreCategoria': [this.categoria.nombreCategoria, [Validators.required]]
     });
   }
+
+  // metodo para agregar una categoria a fire store
+  addcategoria() {
+
+  }
+
+
+  //  // Add a new document with a generated id.
+  // let addDoc = db.collection('cities').add({
+  //   name: 'Tokyo',
+  //   country: 'Japan'
+  // }).then(ref => {
+  //   console.log('Added document with ID: ', ref.id);
+  // });
+
+  // https://www.youtube.com/watch?v=aAssvBo7I1U
+
 
 }
